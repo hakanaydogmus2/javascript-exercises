cd8e3dc871c777aed50562becde5b46cd310f127
Hakan <haydogmus14@outlook.com>
2024-08-01 17:52:42 +0300

    all is well

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeatCount) {
+    if(repeatCount <0){
+        return "ERROR";
+    }
+    let temp = word;
+    for(let i = 1; i<repeatCount; i++){
+        word = word.concat(temp);
+    }
+    return word;
 };
 
 // Do not edit below this line
